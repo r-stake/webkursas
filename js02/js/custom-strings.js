660a4d5f0c6a08a969610a2ba157c7bfc17db792
r-stake <96791353+r-stake@users.noreply.github.com>
2023-05-17 16:08:14 +0300

    custom-strings JavaScript 2 užduotis

--- js02/js/custom-strings.js
+++ js02/js/custom-strings.js
@@ -25,6 +25,10 @@ console.log("Užduotis 02:");
 
 
 
+console.log(aktoriausVardas.toUpperCase() + " " + aktoriausPavarde.toLowerCase());
+
+
+
 /* 03.
 Sukurti du kintamuosius. Jiems priskirti savo mylimo aktoriaus vardą ir pavardę kaip stringus. Sukurti trečią kintamąjį ir jam priskirti stringą, sudarytą iš pirmų vardo ir pavardės kintamųjų raidžių. Jį atspausdinti.
 */
