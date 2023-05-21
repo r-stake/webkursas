4ea458a06f02fc02ef2f0c9d94d213e3d65eaa74
r-stake <96791353+r-stake@users.noreply.github.com>
2023-05-21 16:28:53 +0300

    Testimonials section styling & content interactivity

--- final-project/js/final-project.js
+++ final-project/js/final-project.js
@@ -1,17 +1,20 @@
 function switchContent(index) {
-    // Hide all content elements
-    var contents = document.getElementsByClassName("content");
-    for (var i = 0; i < contents.length; i++) {
-      contents[i].style.display = "none";
-    }
-    
-    // Show the selected content element
-    document.getElementById("content" + (index + 1)).style.display = "block";
-    
-    // Update active menu item
-    var menuItems = document.getElementsByClassName("menu")[0].getElementsByTagName("a");
-    for (var i = 0; i < menuItems.length; i++) {
+  // Hide all content elements
+  var contents = document.getElementsByClassName("content");
+  for (var i = 0; i < contents.length; i++) {
+    contents[i].style.display = "none";
+  }
+  
+  // Show the selected content element
+  document.getElementById("content" + (index + 1)).style.display = "block";
+  
+  // Update active menu item
+  var menuItems = document.getElementsByClassName("journey-menu-item")[0].getElementsByTagName("button");
+  for (var i = 0; i < menuItems.length; i++) {
+    if (i === index) {
+      menuItems[i].classList.add("active");
+    } else {
       menuItems[i].classList.remove("active");
     }
-    menuItems[index].classList.add("active");
   }
+}
